1decd09c3d132a7674003566312a7c14aa97f602
He-Haiyu <42761608+He-Haiyu@users.noreply.github.com>
2019-09-27 23:28:11 +0800

    Update DielecFunSiO2.m

--- DieclctricFunctions/DielecFunSiO2.m
+++ DieclctricFunctions/DielecFunSiO2.m
@@ -8,7 +8,8 @@ c = 3e8; % light speed, in m/s
 lambda = c ./ (omega/2/pi); % wavelength, in m
 lambda = lambda * 1e6; % wavelength, in μm
 
-%%
+%% 
+% 0.21 to 3.7 μm at 20 °C, not sure if can be used at high temperature
 epsi_real = 1 + 0.6961663*lambda.^2./(lambda.^2-0.0684043^2) + ...
 0.4079426*lambda.^2./(lambda.^2-0.1162414^2) + 0.8974794*lambda.^2./(lambda.^2-9.896161^2);
 epsi_imag = 1e-10;
